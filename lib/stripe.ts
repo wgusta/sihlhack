77f8fecf1679996fb563571092a3ddaf031a0086
wgusta <gueney.usta@stud.hslu.ch>
2025-12-17 14:13:30 +0100

    fix: Use default Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -7,10 +7,7 @@ export function getStripe(): Stripe {
     if (!process.env.STRIPE_SECRET_KEY) {
       throw new Error('STRIPE_SECRET_KEY is not set')
     }
-    stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-12-15.clover',
-      typescript: true,
-    })
+    stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
   }
   return stripeInstance
 }
